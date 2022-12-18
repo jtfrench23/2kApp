417cd05c00a3597a425673bd86c7aaaa61522189
Jordan French <jtf981@culver.edu>
2022-12-17 23:48:02 -0600

    Update PersonForm.js

--- client/src/components/PersonForm.js
+++ client/src/components/PersonForm.js
@@ -12,9 +12,9 @@ const PersonForm = () => {
             gamertag
         })
             .then(res=>{
-                console.log(res); // always console log to get used to tracking your data!
-                console.log(res.data);
-            })
+                console.log(res); 
+                console.log(res.data);
+            })
             .catch(err=>console.log(err))
     }
     
@@ -22,9 +22,6 @@ const PersonForm = () => {
         <form onSubmit={onSubmitHandler}>
             <p>
                 <label>Gamertag</label><br/>
-                {/* When the user types in this input, our onChange synthetic event 
-                    runs this arrow function, setting that event's target's (input) 
-                    value (what's typed into the input) to our updated state   */}
                 <input type="text" onChange = {(e)=>setGamerTag(e.target.value)}/>
             </p>
             <input type="submit"/>
