8086bfe8c6fd2f0582e5d8393c30ae92f42ac114
Jordan French <jtf981@culver.edu>
2022-12-19 23:53:42 -0600

    Create game.model.js

---
+++ server/models/game.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const GameSchema = new mongoose.Schema({
+    gamertag: { type: String },
+    win: {type:Boolean},
+    points: {type: Number},
+    assists: {type: Number},
+    steals: {type: Number},
+    rebounds: {type: Number},
+    turnovers: {type: Number},
+    fouls: {type: Number}
+}, { timestamps: true });
+module.exports = mongoose.model('Game', GameSchema);
